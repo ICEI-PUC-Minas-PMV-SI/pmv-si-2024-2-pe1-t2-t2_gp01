587d74b264f219359385228e3d9b59af014ed3e0
Jesse-Cunha <jesse.delgado@hotmail.com>
2024-10-31 22:02:18 -0300

    Método "cadastrar cliente" foi apagado da classe cliente (deve ser construído fora da classe).

--- classe-cliente.js
+++ classe-cliente.js
@@ -9,19 +9,8 @@ export class cliente {
 
     servicos_agendados = []
 
-    // Método que vai cadastrar cada objeto cliente:
-    cadastrar_cliente (nome_,sobrenome_, telefone_, email_, usuario_, senha_){
-
-        this.nome = nome_
-        this.sobrenome = sobrenome_
-        this.telefone = telefone_
-        this.email = email_
-        this.usuario = usuario_
-        this.senha = senha_
-
-    }
-
-    // Método que vai editar os dados de cada objeto cliente
+    
+    // Método que vai editar os dados de um objeto cliente já existente
     editar_cliente (nome_,sobrenome_, telefone_, email_, usuario_){
 
         this.nome = nome_
@@ -32,7 +21,7 @@ export class cliente {
 
     }
 
-    // Método que vai mudar a senha de cada objeto cliente:
+    // Método que vai mudar a senha de um objeto cliente já existente:
     mudar_senha(nova_senha){
 
     this.senha = nova_senha
