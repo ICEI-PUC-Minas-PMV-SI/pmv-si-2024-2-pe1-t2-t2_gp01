55ceca794cae710673014de1d8532b0fe40e02d2
Jesse-Cunha <jesse.delgado@hotmail.com>
2024-11-07 19:22:05 -0300

    Função de cadastrar cliente chamada com sucesso no script.

--- src/js/funcao-cadastrar.js
+++ src/js/funcao-cadastrar.js
@@ -3,18 +3,19 @@ import { classe_cliente } from "./classe-cliente.js";
 
 export function cadastrar_cliente(nome, sobrenome, telefone, email, usuario, senha, repita_senha) {
 
-    if (senha === repita_senha) {
+    // if (senha !== repita_senha) {
 
-         let cliente_1 = new classe_cliente(nome, sobrenome, telefone, email, usuario, senha)
+    //     alert("As senhas digitadas não estão iguais. Repita o procedimento.")
 
-         confirm(`O cliente ${cliente_1.nome} ${cliente_1.sobrenome} foi cadastrado. Telefone: ${cliente_1.telefone}. E-mail: ${cliente_1.email}. Nome de usuário: ${cliente_1.usuario}.`)
+    //     return
+        
+    // } else {
 
-    } else {
+        let cliente_1 = new classe_cliente(nome, sobrenome, telefone, email, usuario, senha)
 
-        alert("As senhas digitadas não estão iguais. Repita o procedimento.")
+        confirm(`O cliente ${cliente_1.nome} ${cliente_1.sobrenome} foi cadastrado. Telefone: ${cliente_1.telefone}. E-mail: ${cliente_1.email}. Nome de usuário: ${cliente_1.usuario}.`)
 
-        return
-    }
+    // }
 
 }
 
